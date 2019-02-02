32e5e0b68f866853476549ec71567ca6221042e1
gregmorihovitis <gregmorihovitis@gmail.com>
2019-02-02 21:08:40 +0000

    began ajax req for resource page

--- public/scripts/resource-page.js
+++ public/scripts/resource-page.js
@@ -1,67 +1,43 @@
-//THIS CODE WAS BASICALLY COPY-PASTED FROM TWEETER APP.JS
-//only replaced tweet with comment - not currently in use
+function createResourceElement(resource) {
+  
+  let $resource =`
+  <article class="resource-container">
+    <!-- Resource includes main image, title, and description -->
+    <img src=${resource.img_url} class="main-img" height="280" width="auto">
+    <!-- Like/Rate buttons-->
+    <section class="like-rate">
+      <button class="btn"><img src="/images/like.png"></button>
+    </section>
+    <!-- Resource titles and description -->
+    <span class="resource-text">
+      <h2>
+        <a href=${resource.url}>${resource.title}</a>
+      </h2>
+      <p>${resource.description}</p>
+    </span>
+    <!-- Comment submission form -->
+    <section class="new-comment">
+      <h4 id="compose-header">Add comment</h4>
+      <form action="/comments" method="POST">
+        <textarea autofocus name="text" placeholder="What do you think?"></textarea>
+        <input type="submit" value="comment">
+      </form>
+    </section>
+    <!-- Comments container: has avatars, usernames, and comment -->
+    <div class="comments-container">
+      <!-- Appends comments from JSON object  -->
+      <% JSON.parse(comments).forEach(function(comment){ %>
+      <section class="unique-comment">
+        <img src="/images/avatar.png" height="40" width="auto" class="avatar">
+        <p class="user-handle">
+          <%= comment.name %>
+        </p>
+        <span class="comment">
+          <%= comment.comment %></span>
+      </section>
+      <% }); %>
 
-
-
-//Prevents cross-site scripting
-function escape(str) {
-  var div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
-
-//fetches new comments from comments page
-function loadComments() {
-  $('.posted-comments').empty();
-  $.ajax({
-    method: "GET",
-    url: "/comments",
-    dataType: "json"
-  })
-    .done(function (commentData) {
-      rendercomments(commentData);
-    })
-}
-
-//Displays all comments
-function renderComments(comments) {
-  // loops through comments
-  for (let comment = 0; comment < comments.length; comment++) {
-    // calls createCommentElement for each comment
-    // takes return value and appends it to the comments container
-    $('.container .posted-comments').prepend(createCommentElement(comments[comment]))
+    </div>
+  </article></div>`  
+    return $resource;
   }
-}
-
-
-//Creates individual comment container (usernamename, avatar, comment, rating) after comment is submitted
-function createCommentElement(comment) {
-  var $commentPassed = `
-  <section class="unique-comment">
-        <img src="/images/avatar.png">
-        <p class=${comment.user.name}</p>
-        <p class=${escape(comment.content.text)}</p>
-      </section>`
-  return $commentPassed;
-}
-
-$(document).ready(function () {
-
-  loadComments();
-
-  // Ajax post request to submit comments
-  $('.new-comment form').on('submit', function (event) {
-    event.preventDefault()
-    //once a form is submitted loadedComments only prepends the most recent post
-    firstLoad = false;
-    $.ajax({
-      method: "POST",
-      url: "/comments",
-      data: $(this).serialize()
-    })
-      .done(function () {
-        loadComments()
-        $('.new-comment form textarea').val('');
-      })
-  });
-});
